ae958ff2a2f22aedba1a333ac1ac698f060273f0
Barry Lee <hcbarry@gmail.com>
2016-04-14 18:01:23 +0800

    Fix `setViewControllers:direction:animated:completion:` crash on UIPageViewController with transition style `UIPageViewControllerTransitionStyleScroll` and animated flag `YES` http://stackoverflow.com/a/17330606

--- GUITabPagerViewController/Classes/GUITabPagerViewController.m
+++ GUITabPagerViewController/Classes/GUITabPagerViewController.m
@@ -97,10 +97,18 @@
       [[self delegate] tabPager:self willTransitionToTabAtIndex:index];
     }
     
+    UIPageViewControllerNavigationDirection direction = (index > [self selectedIndex]) ? UIPageViewControllerNavigationDirectionForward : UIPageViewControllerNavigationDirectionReverse;
+
     [[self pageViewController]  setViewControllers:@[[self viewControllers][index]]
-                                         direction:(index > [self selectedIndex]) ? UIPageViewControllerNavigationDirectionForward : UIPageViewControllerNavigationDirectionReverse
+                                         direction:direction
                                           animated:YES
                                         completion:^(BOOL finished) {
+                                          if (finished) {
+                                            dispatch_async(dispatch_get_main_queue(), ^{
+                                              // bug fix for UIPageViewController
+                                                [self.pageViewController setViewControllers:@[[self viewControllers][index]] direction:direction animated:NO completion:NULL];
+                                            });
+                                          }
                                           [self setSelectedIndex:index];
                                           
                                           if ([[self delegate] respondsToSelector:@selector(tabPager:didTransitionToTabAtIndex:)]) {
